63e99265c54894af7728d851d81084cb624a2c18
Md Raiyan Raza Khan <mdraiyanrazakhan123@gmail.com>
2026-09-23 14:14:44 +0530

    Time: 2 ms (99.91%), Space: 52.5 MB (64.98%) - CodeHub

--- leetcode/0229-majority-element-ii/0229-majority-element-ii.java
+++ leetcode/0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,45 +1,40 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int candidate1 = 0, count1 = 0;
-        int candidate2 = 0, count2 = 0;
-
-        for (int num : nums) {
-            if (count1 > 0 && num == candidate1) {
-                count1++;
-            } else if (count2 > 0 && num == candidate2) {
-                count2++;
-            } else if (count1 == 0) {
-                candidate1 = num;
-                count1 = 1;
-            } else if (count2 == 0) {
-                candidate2 = num;
-                count2 = 1;
-            } else {
-                count1--;
-                count2--;
-            }
+       int c1 = 0, count1 = 0;
+       int c2 = 0, count2 = 0;
+       for(int num : nums) {
+        if(count1 > 0 && num == c1) {
+            count1++;
+        } else if(count2 > 0 && num == c2) {
+            count2++;
+        } else if(count1 == 0) {
+            c1 = num;
+            count1 = 1;
+        } else if(count2 == 0) {
+            c2 = num;
+            count2 = 1;
+        } else {
+            count1--;
+            count2--;
         }
-
-        count1 = 0;
-        count2 = 0;
-        for (int num : nums) {
-            if (num == candidate1) {
-                count1++;
-            } else if (num == candidate2) {
-                count2++;
-            }
+       }
+       count1 = 0;
+       count2 = 0;
+       for(int num : nums) {
+        if(num == c1) {
+            count1++;
+        } else if(num == c2) {
+            count2++;
         }
-
-        List<Integer> result = new ArrayList<>();
-        int threshold = nums.length / 3;
-
-        if (count1 > threshold) {
-            result.add(candidate1);
-        }
-        if (count2 > threshold) {
-            result.add(candidate2);
-        }
-
-        return result;
+       }
+       List<Integer> result = new ArrayList<>();
+       int threshold = nums.length/3;
+       if(count1 > threshold) {
+        result.add(c1);
+       }
+       if(count2 > threshold) {
+        result.add(c2);
+       }
+       return result;
     }
 }
